7f65dfe35496a88b59a29999cab7ed7107f6bd53
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2024-03-16 13:26:53 +0200

    comlited

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Description from "./components/description/Description.jsx";
 import Feedback from "./components/feedback/Feedback.jsx";
 import Notification from "./components/notification/Notification.jsx";
 import Options from "./components/options/Options.jsx";
 
-
-
+const objFeedbacks = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+}
 const App = () => {
-  const [checkFeedbacks, setCheckFeedbacks] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0
+
+  const [checkFeedbacks, setCheckFeedbacks] = useState(() => {
+    const parseFeedbacks = JSON.parse(localStorage.getItem("feedbacks") ?? objFeedbacks);
+    return parseFeedbacks;
   });
 
   const updateFeedback = (feedbackType) => {
@@ -28,19 +31,21 @@ const App = () => {
     })
   }
 
-  const totalFeedbacks = checkFeedbacks.good + checkFeedbacks.neutral + checkFeedbacks.bad;
+  const totalFeedbacks = Object.values(checkFeedbacks).reduce((acc, curr) => acc + curr, 0);
   const positive = Math.round(((checkFeedbacks.good + checkFeedbacks.neutral) / totalFeedbacks) * 100);
+
+  useEffect(() => {
+    localStorage.setItem("feedbacks", JSON.stringify(checkFeedbacks))
+  }, [checkFeedbacks])
 
   return (
     <>
       <Description />
-    
-        <Options
-          handleClick={updateFeedback}
-          handleClickReset={handleClickReset}
-          totalFeedbacks={totalFeedbacks}
-        /> 
-        
+      <Options
+        handleClick={updateFeedback}
+        handleClickReset={handleClickReset}
+        totalFeedbacks={totalFeedbacks}
+      />
       {totalFeedbacks ?
         <Feedback
           checkFeedbacks={checkFeedbacks}
@@ -54,8 +59,3 @@ const App = () => {
 };
 
 export default App;
-//  {/* <Options
-      //     handleClick={updateFeedback}
-      //     handleClickReset={handleClickReset}
-      //   />
-      // }
